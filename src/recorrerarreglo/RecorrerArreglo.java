aa0ad41a168c74f88263107809f9d71de34939a8
Bryanbsm <32758195+Bryanbsm@users.noreply.github.com>
2019-02-22 21:23:09 -0500

    Add files via upload

---
+++ src/recorrerarreglo/RecorrerArreglo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recorrerarreglo;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class RecorrerArreglo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+   /*
+   ClaseArray a = new ClaseArray();
+        a.agregarDatos(500000);
+       // a.recorrerFOR();
+        a.recorrerForEach();
+        */
+   
+   InterfazArreglo ia = new InterfazArreglo();
+   ia.setVisible(true);
+    }
+    
+}
